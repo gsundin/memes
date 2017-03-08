6a24fb0c6e304197003d0da4318c494dacf49c99
wuxx1045 <wuxx1045@umn.edu>
2017-03-08 12:41:28 -0600

    added config so app secret isnt in server.js

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 var express = require("express");
 var app = express();
 
+var config = require('./fb.js');
+
 var FB = require('fb');
 var fb = new FB.Facebook();
 
 // Instance of our Facebook app, called "meme-stream"
-var memeApp = FB.extend({appId: '1849212205294114', appSecret: '2c90cfcdd707c1bfcd0396068c923511'})
+var memeApp = FB.extend({appId: config.CLIENT_ID, appSecret: config.CLIENT_SECRET});
 
 /* serves main page */
 app.get("/", function(req, res) {
 	res.sendfile('redirect.htm')
 });
 
-app.post("/user/add", function(req, res) { 
+app.post("/user/add", function(req, res) {
 	/* some server side logic */
 	res.send("OK");
 });
 
 /* serves all the static files */
-app.get(/^(.+)$/, function(req, res){ 
+app.get(/^(.+)$/, function(req, res){
 	console.log('static file request : ' + req.params);
-	res.sendfile( __dirname + req.params[0]); 
+	res.sendfile( __dirname + req.params[0]);
 });
 
 var port = process.env.PORT || 8080;
