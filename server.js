400a283754c46a27c87abed66d9ed8691141c037
Garrett Sundin <sundi024@umn.edu>
2017-03-07 23:18:27 -0600

    add: Instance of FB plugin within server

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
- var express = require("express");
- var app = express();
+var express = require("express");
+var app = express();
 
- /* serves main page */
- app.get("/", function(req, res) {
-    res.sendfile('redirect.htm')
- });
+var FB = require('fb');
+var fb = new FB.Facebook();
 
-app.post("/user/add", function(req, res) { 
-  /* some server side logic */
-  res.send("OK");
+// Instance of our Facebook app, called "meme-stream"
+var memeApp = FB.extend({appId: '1849212205294114', appSecret: '2c90cfcdd707c1bfcd0396068c923511'})
+
+/* serves main page */
+app.get("/", function(req, res) {
+	res.sendfile('redirect.htm')
 });
 
- /* serves all the static files */
- app.get(/^(.+)$/, function(req, res){ 
-     console.log('static file request : ' + req.params);
-     res.sendfile( __dirname + req.params[0]); 
- });
+app.post("/user/add", function(req, res) { 
+	/* some server side logic */
+	res.send("OK");
+});
 
- var port = process.env.PORT || 8080;
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){ 
+	console.log('static file request : ' + req.params);
+	res.sendfile( __dirname + req.params[0]); 
+});
 
- app.listen(port, function() {
-   console.log("Listening on " + port);
- });
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
